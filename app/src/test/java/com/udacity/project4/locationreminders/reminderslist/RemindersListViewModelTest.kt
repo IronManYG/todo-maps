141081671c455e6366087878e2829f68c68dd3a1
IronManYG <hussain.yg2010@gmail.com>
2021-09-16 16:40:12 +0300

    Start Testing

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,7 +1,23 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.FirebaseApp
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.core.IsNot
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -10,4 +26,65 @@ class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
+    // Use a fake DataSource to be injected into the viewmodel
+    private lateinit var dataSource: FakeDataSource
+
+    // Subject under test
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    private val reminder1 = ReminderDTO("Title1", "Description1","Location1", 1.0,1.0)
+
+    private val reminder2 = ReminderDTO("Title2", "Description2","Location2", 2.0,2.0)
+
+    private val reminder3 = ReminderDTO("Title1", "Description1","Location1", 3.0,3.0)
+
+    @Before
+    fun setupFirebase() {
+        // initialize the firebase
+        FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext())
+    }
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Test
+    fun loadReminders_getsRemindersListForPopulatedRemindersList(){
+        // Given  a data source containing a reminders list
+        val localReminders = listOf(reminder1,reminder2,reminder3).sortedBy { it.id }
+
+        dataSource = FakeDataSource(localReminders.toMutableList())
+
+        // Given a fresh RemindersListViewModel
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(),dataSource)
+
+        // When load reminders from data source
+        remindersListViewModel.loadReminders()
+
+        // Then remindersList is not empty
+        val value = remindersListViewModel.remindersList.getOrAwaitValue()
+
+        assertThat(value, (not(emptyList())))
+    }
+
+    @Test
+    fun loadReminders_returnsErrorForNullRemindersList() {
+        // Given a data source containing null
+        dataSource=FakeDataSource(null)
+
+        // Given a fresh RemindersListViewModel
+        remindersListViewModel=
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+
+        // When loading reminders
+        remindersListViewModel.loadReminders()
+
+        // Then a snack bar with the message "No reminders found" is shown
+        val value = remindersListViewModel.showSnackBar.getOrAwaitValue()
+
+        MatcherAssert.assertThat(value,`is`("Reminders not found!"))
+    }
 }
